d35110b4d611c0d9b7a4a044aa0c9c808ffd1998
Attabik Shah <79091026+attabik-shah@users.noreply.github.com>
2024-02-03 02:27:22 +0500

    Add files via upload

---
+++ client/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout title={"About us - Colossal Store"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+          This is our Final year project using mern stack and implementing virtual try on and visual search as our main feature.
+           we are 4 group members Syed Attabik Ali Shah, Muhammad Uzair, Sheraz Hussain, Muhammad Rizwan Hameed. 
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
